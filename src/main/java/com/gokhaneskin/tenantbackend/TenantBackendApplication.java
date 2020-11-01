91bb93ee2477dca868dccc56cfe9ce3fac9fdfd6
gokhan_eskin <gokhaneskin34@gmail.com>
2020-11-01 17:28:02 +0300

    first commit

---
+++ src/main/java/com/gokhaneskin/tenantbackend/TenantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gokhaneskin.tenantbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenantBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gokhaneskin/tenantbackend/TenantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokhaneskin.tenantbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenantBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
